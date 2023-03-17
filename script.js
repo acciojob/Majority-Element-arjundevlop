34271e671b0344bb29ff804d02e6369197453759
Arjun Singh Tomar <109807311+arjundevlop@users.noreply.github.com>
2023-03-17 15:32:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your code here
+var arr = [2, 1, 2];
+var n = arr.length;
+let majElem;
+for(var i=0; i<arr.length; i++)
+	{
+		var count = 0;
+		for(var j=0; j<arr.length; j++)
+			{
+				if(arr[j]==arr[i])
+				{
+					count++;
+					if(count>Math.floor(n/2))
+					{
+						majElem = arr[i];
+					}
+				}
+			}
+	}
+console.log(majElem);
